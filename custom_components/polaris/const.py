9bdbf9a18274b80c579cd456c42c511c9391014d
samoswall <80251553+samoswall@users.noreply.github.com>
2024-02-01 00:01:40 +0300

    Create const.py

---
+++ custom_components/polaris/const.py
@@ -0,0 +1,10 @@
+from itertools import cycle, islice
+from typing import Final
+
+DOMAIN: Final = "Polaris"
+
+ATTR_NAME = "name"
+ATTR_ERROR = "error"
+ATTR_STATE = "state"
+
+CONF_TOPIC_PREFIX = "topic_prefix"
